61a5c6f7b963b4b35d7c87cbbf62a7d219dc51fc
Oscar Koeroo <okoeroo@gmail.com>
2013-06-01 23:04:36 +0200

    Removed the method of using the aux and now using the argc/argv to initialize a different URL.

--- simple_curl_call.c
+++ simple_curl_call.c
@@ -14,7 +14,7 @@ example_plugin_init(tq_xacml_callout_t *callout) {
     int i;
     int argc;
     char **argv;
-    char *test;
+    /* char *test; */
 
     argc = genauthz_callout_get_argc(callout);
     argv = genauthz_callout_get_argv(callout);
@@ -24,14 +24,14 @@ example_plugin_init(tq_xacml_callout_t *callout) {
     }
 
     /* test = strdup("http://www.slashdot.com"); */
-    test = strdup("http://localhost/verlanglijstje/");
-    genauthz_callout_set_aux(callout, test);
+    /* test = strdup("http://localhost/verlanglijstje/"); */
+    /* genauthz_callout_set_aux(callout, test); */
     return 0;
 }
 
 void
 example_plugin_uninit(tq_xacml_callout_t *callout) {
-    printf("%s\n", (char *)genauthz_callout_get_aux(callout));
+    printf("%p\n", (void *)callout);
     return;
 }
 
@@ -41,18 +41,30 @@ example_plugin_rule_hit(request_mngr_t *request_mngr,
                         tq_xacml_callout_t *callout) {
     CURL *curl;
     CURLcode res;
+    int argc;
+    char **argv;
+    int i;
+
+    argc = genauthz_callout_get_argc(callout);
+    argv = genauthz_callout_get_argv(callout);
 
     printf("Rule \"%s\" hit! -- %s\n", rule->name, __func__);
+    for (i = 0; i < argc; i++) {
+        printf("Argv[%d]: %s\n", i, argv[i]);
+    }
 
     print_normalized_xacml_request(request_mngr->xacml_req);
     print_normalized_xacml_response(request_mngr->xacml_res);
     print_loaded_policy(request_mngr->app->parent->xacml_policy);
 
-    printf("%s\n", (char *)genauthz_callout_get_aux(callout));
+    /* printf("%s\n", (char *)genauthz_callout_get_aux(callout)); */
+    if (argc != 2)
+        return 1;
 
     curl = curl_easy_init();
     if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, (char *)genauthz_callout_get_aux(callout));
+        printf("---------------> %s\n", argv[1]);
+        curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
         /* example.com is redirected, so we tell libcurl to follow redirection */
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
