715872cd7122fb1d97c7e07f33834700be183471
Oscar Koeroo <okoeroo@gmail.com>
2013-06-01 14:17:35 +0200

    Works similar to the example plugin

--- simple_curl_call.c
+++ simple_curl_call.c
@@ -1,4 +1,11 @@
-#include "example_main.h"
+#include "genauthz/genauthz_plugin.h"
+
+int
+example_plugin_init(tq_xacml_callout_t *);
+void
+example_plugin_uninit(tq_xacml_callout_t *);
+void
+example_plugin_rule_hit(request_mngr_t *, tq_xacml_rule_t *, tq_xacml_callout_t *);
 
 
 int
