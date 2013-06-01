97192947e3dd2022cb47f4bb074cbeba7ac8359c
Oscar Koeroo <okoeroo@gmail.com>
2013-06-01 15:16:51 +0200

    Functional, but very very expensive way of testing the plugin in the GA service

--- simple_curl_call.c
+++ simple_curl_call.c
@@ -1,4 +1,5 @@
 #include "genauthz/genauthz_plugin.h"
+#include <curl/curl.h>
 
 int
 example_plugin_init(tq_xacml_callout_t *);
@@ -22,7 +23,7 @@ example_plugin_init(tq_xacml_callout_t *callout) {
         printf("Argv[%d]: %s\n", i, argv[i]);
     }
 
-    test = strdup("w00t w00t");
+    test = strdup("http://www.slashdot.com");
     genauthz_callout_set_aux(callout, test);
     return 0;
 }
@@ -37,6 +38,9 @@ void
 example_plugin_rule_hit(request_mngr_t *request_mngr,
                         tq_xacml_rule_t *rule,
                         tq_xacml_callout_t *callout) {
+    CURL *curl;
+    CURLcode res;
+
     printf("Rule \"%s\" hit! -- %s\n", rule->name, __func__);
 
     print_normalized_xacml_request(request_mngr->xacml_req);
@@ -44,6 +48,23 @@ example_plugin_rule_hit(request_mngr_t *request_mngr,
     print_loaded_policy(request_mngr->app->parent->xacml_policy);
 
     printf("%s\n", (char *)genauthz_callout_get_aux(callout));
+
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, (char *)genauthz_callout_get_aux(callout));
+        /* example.com is redirected, so we tell libcurl to follow redirection */
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        /* Perform the request, res will get the return code */
+        res = curl_easy_perform(curl);
+        /* Check for errors */
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+    }
 
     return;
 }
